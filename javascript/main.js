64e87b97aff441ae6aa667a4c8ac8f4f30c05d05
Michael Yang <themichaelyang@gmail.com>
2017-04-02 23:02:13 -0400

    added spacing

--- javascript/main.js
+++ javascript/main.js
@@ -3,12 +3,14 @@ let columns = 3;
 let length = rows * columns;
 
 function main() {
+  let gallery = document.getElementById('videos');
+
   for (let i = 0; i < length; i++) {
     let video = document.createElement('video');
     video.src = 'videos/' + '0' + (i + 1).toString() + '.mp4';
     video.autoplay = true;
     video.loop = true;
-    document.body.appendChild(video);
+    gallery.appendChild(video);
   }
 }
 
